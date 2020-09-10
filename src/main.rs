21570d437b770e6d06c04fc5e42be44d0c8dca99
Daiki Ueno <dueno@redhat.com>
2020-09-10 20:00:44 +0200

    Add support for reading Wasm workload from file descriptor

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,23 @@
 //! ## Example invocation
 //!
 //! ```console
-//! $ RUST_LOG=keep_runtime=info RUST_BACKTRACE=1 cargo run target/debug/fixtures/return_1.wasm
-//!    Compiling keep-runtime v0.1.0 (/home/steveej/src/job-redhat/enarx/github_enarx_enarx/keep-runtime)
-//!     Finished dev [unoptimized + debuginfo] target(s) in 4.36s
-//!      Running `target/debug/keep-runtime`
-//! [2020-01-23T21:58:16Z INFO  keep_runtime] got result: [
+//! $ wat2wasm fixtures/return_1.wat
+//! $ RUST_LOG=enarx_wasmldr=info RUST_BACKTRACE=1 cargo run return_1.wasm
+//!     Finished dev [unoptimized + debuginfo] target(s) in 0.07s
+//!      Running `target/x86_64-unknown-linux-musl/debug/enarx-wasmldr target/x86_64-unknown-linux-musl/debug/build/enarx-wasmldr-c374d181f6abdda0/out/fixtures/return_1.wasm`
+//! [2020-09-10T17:56:18Z INFO  enarx_wasmldr] got result: [
 //!         I32(
 //!             1,
 //!         ),
 //!     ]
 //! ```
+//!
+//! On Unix platforms, the command can also read the workload from the
+//! file descriptor (3):
+//! ```console
+//! $ RUST_LOG=enarx_wasmldr=info RUST_BACKTRACE=1 cargo run 3< return_1.wasm
+//! ```
+//!
 #![deny(missing_docs)]
 #![deny(clippy::all)]
 
@@ -26,16 +33,39 @@ mod config;
 mod virtfs;
 mod workload;
 
+use cfg_if::cfg_if;
 use log::info;
+
+use std::fs::File;
+use std::io::Read;
+#[cfg(unix)]
+use std::os::unix::io::FromRawFd;
+
+#[cfg(unix)]
+const FD: std::os::unix::io::RawFd = 3;
 
 fn main() {
     let _ = env_logger::try_init_from_env(env_logger::Env::default());
 
     let mut args = std::env::args().skip(1);
-    let path = args.next().unwrap();
     let vars = std::env::vars();
 
-    let bytes = std::fs::read(&path).expect("Unable to open file");
+    let mut reader = if let Some(path) = args.next() {
+        File::open(&path).expect("Unable to open file")
+    } else {
+        cfg_if! {
+            if #[cfg(unix)] {
+                unsafe { File::from_raw_fd(FD) }
+            } else {
+                unreachable!();
+            }
+        }
+    };
+
+    let mut bytes = Vec::new();
+    reader
+        .read_to_end(&mut bytes)
+        .expect("Failed to load workload");
 
     let result = workload::run(&bytes, args, vars).expect("Failed to run workload");
 
