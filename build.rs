45e36f84d1b4e843ae10b8bd9829713d93f2ac57
Daiki Ueno <dueno@redhat.com>
2020-09-21 17:57:50 +0200

    Add unit tests for running bundled Wasm binary
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let in_dir = Path::new("fixtures");
@@ -18,7 +19,34 @@ fn main() {
                         .join(wat.file_name().unwrap())
                         .with_extension("wasm");
                     let binary = wat::parse_file(&wat).expect("Can't parse wat file");
-                    std::fs::write(wasm, &binary).expect("Can't write wasm file");
+                    std::fs::write(&wasm, &binary).expect("Can't write wasm file");
+                    // If the "enarx" command is installed, create a
+                    // bundled Wasm file for testing.
+                    let status = Command::new("enarx")
+                        .arg("wasm")
+                        .arg("bundle")
+                        .arg("fixtures/bundle")
+                        .arg(&wasm)
+                        .arg(&wasm.with_extension("bundled.wasm"))
+                        .status();
+
+                    if let Ok(status) = status {
+                        if !status.success() {
+                            println!(
+                                "cargo:warning=Error bundling resources for {:?}: {}",
+                                &wasm.file_name().unwrap(),
+                                status.code().unwrap()
+                            );
+                        } else {
+                            println!("cargo:rustc-cfg=bundle_tests");
+                        }
+                    } else {
+                        println!(
+                            "cargo:warning=Not bundling resources for {:?}",
+                            &wasm.file_name().unwrap()
+                        );
+                    }
+
                     println!("cargo:rerun-if-changed={}", &wat.display());
                 }
                 _ => {}
