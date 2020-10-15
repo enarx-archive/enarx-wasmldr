enarx-archive/enarx-wasmldr

commit c6edd1b7d7d4bdcd1995554ab7a4b1de084bdea3

Elide enarx-keepldr argument
enarx-keepldr commit f0d9c7e39eb29d084be394d9a1c38b399ae15496
injects the enarx-keepldr image name into the process command line
arguments to the exec syscall in the event the nil backend is selected.

This accidentally pollutes the arguments list that enarx-wasmldr
passes to the workload run function, as enarx-wasmldr only skips 1
argument because it expects the first argument is simply the path
to the enarx-wasmldr image.

This patch updates the arg skipping logic to account for an
enarx-keepldr nil backend launch so we don't accidentally pollute
the arguments list passed to the workload run function and crash:

$ ENARX_BACKEND=nil 3<>demo.wasm ./enarx-keepldr exec enarx-wasmldr
thread 'main' panicked at 'Failed to run workload: IoError(Kind(InvalidInput))', src/main.rs:70:52
note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace

Signed-off-by: Connor Kuehl <ckuehl@redhat.com>

src/main.rs
@@ -47,7 +47,8 @@ const FD: std::os::unix::io::RawFd = 3;
 fn main() {
     let _ = env_logger::try_init_from_env(env_logger::Env::default());
 
-    let mut args = std::env::args().skip(1);
+    let mut args = std::env::args()
+        .skip_while(|s| s.ends_with("enarx-keepldr") || s.ends_with("enarx-wasmldr"));
     let vars = std::env::vars();
 
     let mut reader = if let Some(path) = args.next() {

commit 8851173a12344378ac61c014510e60da92a8f40c

Remove assumption about command line arguments
The KVM shim will launch the enarx-wasmldr as "/init", so
the original "fix" ended up regressing KVM-based backends.

This returns to the original behavior and simply increments
the number of elements to skip if the "enarx-keepldr" image
path precedes the regular command line arguments.

Fixes: c6edd1b7d7d4bdcd1995554ab7a4b1de084bdea3

Signed-off-by: Connor Kuehl <ckuehl@redhat.com>

src/main.rs
@@ -47,8 +47,13 @@ const FD: std::os::unix::io::RawFd = 3;
 fn main() {
     let _ = env_logger::try_init_from_env(env_logger::Env::default());
 
-    let mut args = std::env::args()
-        .skip_while(|s| s.ends_with("enarx-keepldr") || s.ends_with("enarx-wasmldr"));
+    // Skip the program name by default, but also skip the enarx-keepldr image
+    // name if it happens to precede the regular command line arguments.
+    let nskip = 1 + std::env::args()
+        .take(1)
+        .filter(|s| s.ends_with("enarx-keepldr"))
+        .count();
+    let mut args = std::env::args().skip(nskip);
     let vars = std::env::vars();
 
     let mut reader = if let Some(path) = args.next() {