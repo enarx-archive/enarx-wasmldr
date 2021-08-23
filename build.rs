8673bbed491815eaf6e6e0b63e3a87292b0ceb3d
Will Woods <will@congru.us>
2021-08-23 15:48:33 -0400

    Rework for wasmtime 0.29 and WASI changes
    A lot has changed in upstream wasmtime / WASI, so wasmldr needs
    significant reworking to keep up.
    
    The biggest change upstream is that virtfs has been dropped from
    wasi_common. So we're dropping the bundle format and virtual filesystem
    view, for now, and we'll reimplement that functionality piece by piece,
    using things that are a little better aligned with current directions in
    WASI and WebAssembly.
    
    With this commit, enarx-wasmldr at least compiles and passes the remaining
    tests, and can successfully run some random wasm modules I had laying
    around. So this seems like a good start.
    
    Detailed changes:
    
    * bumped deps to wasmtime 0.29 and wasmparser 0.80
    * removed bundle stuff from build.rs
    * removed config.rs and virtfs.rs
    * removed deps on serde, tar, tempfile from Cargo.toml
    * remove populate_virtfs() etc. from workload.rs
      * NOTE: this means we can't load modules on FD3 right now (that will
        be restored in a future commit)
    * moved CLI parsing into cli.rs, since this might get reused elsewhere
    * switched to structopt for CLI parsing, as we do elsewhere
    * use anyhow for error handling (like wasmtime does)
    * tweak trait bounds on workload::run() to work better with args/envs
      from structopt
    * enable module-linking and multi-memory in instantiated wasmtime engine
    
    Signed-off-by: Will Woods <will@congru.us>

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::path::Path;
-use std::process::Command;
 
 fn main() {
     let in_dir = Path::new("fixtures");
@@ -10,47 +9,18 @@ fn main() {
     let out_dir = Path::new(&out_dir).join("fixtures");
     std::fs::create_dir_all(&out_dir).expect("Can't create output directory");
 
-    for entry in in_dir.read_dir().unwrap() {
-        if let Ok(entry) = entry {
-            let wat = entry.path();
-            match wat.extension() {
-                Some(ext) if ext == "wat" => {
-                    let wasm = out_dir
-                        .join(wat.file_name().unwrap())
-                        .with_extension("wasm");
-                    let binary = wat::parse_file(&wat).expect("Can't parse wat file");
-                    std::fs::write(&wasm, &binary).expect("Can't write wasm file");
-                    // If the "enarx" command is installed, create a
-                    // bundled Wasm file for testing.
-                    let status = Command::new("enarx")
-                        .arg("wasm")
-                        .arg("bundle")
-                        .arg("fixtures/bundle")
-                        .arg(&wasm)
-                        .arg(&wasm.with_extension("bundled.wasm"))
-                        .status();
-
-                    if let Ok(status) = status {
-                        if !status.success() {
-                            println!(
-                                "cargo:warning=Error bundling resources for {:?}: {}",
-                                &wasm.file_name().unwrap(),
-                                status.code().unwrap()
-                            );
-                        } else {
-                            println!("cargo:rustc-cfg=bundle_tests");
-                        }
-                    } else {
-                        println!(
-                            "cargo:warning=Not bundling resources for {:?}",
-                            &wasm.file_name().unwrap()
-                        );
-                    }
-
-                    println!("cargo:rerun-if-changed={}", &wat.display());
-                }
-                _ => {}
+    for entry in in_dir.read_dir().unwrap().flatten() {
+        let wat = entry.path();
+        match wat.extension() {
+            Some(ext) if ext == "wat" => {
+                let wasm = out_dir
+                    .join(wat.file_name().unwrap())
+                    .with_extension("wasm");
+                let binary = wat::parse_file(&wat).expect("Can't parse wat file");
+                std::fs::write(&wasm, &binary).expect("Can't write wasm file");
+                println!("cargo:rerun-if-changed={}", &wat.display());
             }
+            _ => {}
         }
     }
 }
